35293df0af18302386cbfa70f1f63a71ce948edb
Cinnabar-coder <2242176931@qq.com>
2023-01-13 22:22:44 +0800

    FirstJava

---
+++ contacts/src/wzh/contacts/hello.java
@@ -0,0 +1,22 @@
+package wzh.contacts;
+/**
+ * 
+ * @Description 
+ * @author cinnabar Email:2242176931@.qq.com
+ * @version
+ * @date 2023年1月8日下午9:13:05
+ *
+ */
+public class hello {
+	/**
+	 * 
+	 * @Description 
+	 * @author cinnabar 
+	 * @date 2023年1月8日下午9:15:35
+	 * @param a
+	 * @return
+	 */
+	public int getnumber(int a) {
+		return 0;
+	}
+}
---
+++ contacts/src/wzh/contacts/helloworld.java
@@ -0,0 +1,7 @@
+package wzh.contacts;
+
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+}
